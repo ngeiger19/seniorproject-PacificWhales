514c3ded1f6a8837241c2dea0459a55fceca7e79
lawlouis <46467488+lawlouis@users.noreply.github.com>
2020-01-30 14:47:26 -0800

    modified two _layout and _loginpartial.cshtml

--- Milestone 3/class_project/Models/IdentityModels.cs
+++ Milestone 3/class_project/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace class_project.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-        /*: base("DefaultConnection", throwIfV1Schema: false)*/
+        /*: base("ClassprojectContext", throwIfV1Schema: false)*/
         : base("ClassprojectContext_Azure", throwIfV1Schema: false)
         {
             // Disable code-first migrations
