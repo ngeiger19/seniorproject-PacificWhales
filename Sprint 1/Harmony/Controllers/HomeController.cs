14f4ee949632da5b1b56bf73e48c582265c56ce8
Nicole Geiger <nmgeiger19@mail.wou.edu>
2020-02-19 19:11:16 -0800

    added linq query; display results on page

--- Sprint 1/Harmony/Controllers/HomeController.cs
+++ Sprint 1/Harmony/Controllers/HomeController.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
+using System.Data;
+using Harmony.Models;
+using Harmony.DAL;
+using System.Net;
 
 namespace Harmony.Controllers
 {
     public class HomeController : Controller
     {
+
+        private HarmonyContext db = new HarmonyContext();
+
         public ActionResult Index()
         {
             return View();
@@ -34,7 +42,19 @@ namespace Harmony.Controllers
 
         public ActionResult Search()
         {
-            return View();
+            string search = Request.QueryString["search"];
+
+            if (search == null || search == "")
+            {
+                return View();
+            }
+
+            var userQuery =
+                from user in db.Users
+                where user.FirstName.Contains(search)
+                select user;
+
+            return View(userQuery);
         }
     }
 }
