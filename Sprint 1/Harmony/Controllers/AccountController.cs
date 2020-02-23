e0541026fcad57b566f37fef8a1a7f26ddf396f4
lawlouis <46467488+lawlouis@users.noreply.github.com>
2020-02-23 03:00:47 -0800

    dropdownlist for US states from a json file

--- Sprint 1/Harmony/Controllers/AccountController.cs
+++ Sprint 1/Harmony/Controllers/AccountController.cs
@@ -9,7 +9,6 @@ using System.IO;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
-using Newtonsoft.Json;
 using Harmony.DAL;
 using Harmony.Models;
 using System.Collections.Generic;
