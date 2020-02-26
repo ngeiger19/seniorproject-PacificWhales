1ab3c50ecc180ad5cad8b2ad8d510ef285f11894
lawlouis <46467488+lawlouis@users.noreply.github.com>
2020-02-25 16:06:50 -0800

    changed context and fixed merge conflict

--- Sprint 1/Harmony/Startup.cs
+++ Sprint 1/Harmony/Startup.cs
@@ -24,11 +24,7 @@ namespace Harmony
             var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
             var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
 
-<<<<<<< HEAD
-            
             // Create a venue owner role   
-=======
->>>>>>> dev
             if (!roleManager.RoleExists("VenueOwner"))
             {                
                 var role = new IdentityRole();
