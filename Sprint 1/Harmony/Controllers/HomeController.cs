e47c156e0355f02e35120ca5d4d628209c895fdd
Nicole Geiger <nmgeiger19@mail.wou.edu>
2020-04-07 15:22:54 -0700

    added linq queries

--- Sprint 1/Harmony/Controllers/HomeController.cs
+++ Sprint 1/Harmony/Controllers/HomeController.cs
@@ -8,6 +8,8 @@ using System.Data;
 using Harmony.Models;
 using Harmony.DAL;
 using System.Net;
+using Newtonsoft.Json.Linq;
+using System.IO;
 
 namespace Harmony.Controllers
 {
@@ -15,7 +17,6 @@ namespace Harmony.Controllers
     {
 
         private HarmonyContext db = new HarmonyContext();
-
         
         public ActionResult Index()
         {
@@ -46,8 +47,24 @@ namespace Harmony.Controllers
         [HttpGet]
         public ActionResult Search(string searchOption)
         {
+            // For state dropdown list
+            StreamReader sr = new StreamReader(Server.MapPath("~/Content/states_hash.json"));
+            string data = sr.ReadToEnd();
+            JArray arr = JArray.Parse(data);
+            List<SelectListItem> stateList = new List<SelectListItem>();
+            for (int i = 0; i < arr.Count(); i++)
+            {
+                stateList.Add(new SelectListItem { Text = (string)arr[i]["name"], Value = (string)arr[i]["name"] });
+            }
+            ViewData.Clear();
+            // ViewBag.State = stateList;
+            ViewData["State"] = stateList;
+            sr.Dispose();
+
             string search = Request.QueryString["search"];
-            
+            string cityFilter = Request.QueryString["cityFilter"];
+            string stateFilter = Request.QueryString["stateFilter"];
+
             // If nothing was typed into search bar
             if (search == null || search == "")
             {
@@ -62,21 +79,51 @@ namespace Harmony.Controllers
             else if (searchOption == "option2")
             {
 
-                return RedirectToAction("VenueSearchResults", new { venueSearch = search });
+                return RedirectToAction("VenueSearchResults", new { venueSearch = search , city = cityFilter, state = stateFilter});
             }
 
             return View();
 
         }
-        
+
         // VENUE SEARCH RESULTS
-        public ActionResult VenueSearchResults(string venueSearch)
+        public ActionResult VenueSearchResults(string venueSearch, string city, string state)
         {
             var venueQuery =
                 from venue in db.Venues
                 where venue.VenueName.Contains(venueSearch)
                 select venue;
 
+            // City filter active
+            if (city != null)
+            {
+                var cityQuery =
+                from venue in venueQuery
+                where venue.City == city
+                select venue;
+
+                // State and City filters active
+                if (state != null)
+                {
+                    var stateQuery =
+                        from venue in cityQuery
+                        where venue.State == state
+                        select venue;
+                    return View(stateQuery);
+                }
+                return View(cityQuery);
+            }
+            // State filter active
+            else if (state != null)
+            {
+                var stateQuery =
+                    from venue in venueQuery
+                    where venue.State == state
+                    select venue;
+                return View(stateQuery);
+            }
+
+            // No filters active
             return View(venueQuery);
         }
 
