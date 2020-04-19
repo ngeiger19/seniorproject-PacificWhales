c0984ad3a27a443f8162a0999121e110ccca441d
Nicole Geiger <nmgeiger19@mail.wou.edu>
2020-04-18 20:49:06 -0700

    created user_show viewmodel

--- Sprint 1/Harmony/Models/ShowsViewModel.cs
+++ Sprint 1/Harmony/Models/ShowsViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Google.GData.Extensions;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,40 @@ namespace Harmony.Models
 {
     public class ShowsViewModel
     {
+        public ShowsViewModel(User_Show show)
+        {
+            MusicianID = show.MusicianID;
+            VenueID = show.VenueOwnerID;
+            ShowID = show.ShowID;
+            StartTime = show.Show.StartDateTime;
+            EndTime = show.Show.EndDateTime;
+            DateBooked = show.Show.DateBooked;
+        }
+
+        public int ID { get; set; }
+
+        [Required]
+        [Display(Name = "Musician ID")]
+        public int MusicianID { get; set; }
+
+        [Required]
+        [Display(Name = "Venue ID")]
+        public int VenueID { get; set; }
+
+        [Required]
+        [Display(Name = "Show ID")]
+        public int ShowID { get; set; }
+
+        [Required]
+        [Display(Name = "Start Date")]
+        public DateTime StartTime { get; set; }
+
+        [Required]
+        [Display(Name = "End Date")]
+        public DateTime EndTime { get; set; }
+
+        [Required]
+        [Display(Name = "Date Booked")]
+        public DateTime DateBooked { get; set; }
     }
 }
