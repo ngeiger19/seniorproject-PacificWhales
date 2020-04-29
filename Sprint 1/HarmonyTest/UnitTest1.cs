ace8543baf052d45749deef745365e851772aa80
Penny <xyang16@wou.edu>
2020-04-29 10:48:54 -0700

    fix merge conflicts

--- Sprint 1/HarmonyTest/UnitTest1.cs
+++ Sprint 1/HarmonyTest/UnitTest1.cs
@@ -22,37 +22,25 @@ namespace HarmonyTest
             AccountController Controller = new AccountController();
             List<SelectListItem> stateList = Controller.ParseJsonStringOfStatesToSelectList(statesJSON);
             int expectedResult = 4;
-            Assert.AreEqual(expectedResult,  stateList.Count());
+            Assert.AreEqual(expectedResult, stateList.Count());
         }
 
         [TestMethod]
         public bool GenerateVenueList_IsEmpty_ReturnsTrue()
         {
             UsersController Controller = new UsersController();
-            
+
             return true;
         }
-<<<<<<< HEAD
-        [TestMethod]
-        public void Total_Add10to10ShouldEqual20()
-        {
-            int x = 10;
-            int y = x + 10;
-            Assert.AreEqual(x + 10, y);
-        }
-
-
-=======
 
         [TestMethod]
         public void CityQuery_EmptyList_ReturnsEmptyList()
         {
             HomeController controller = new HomeController();
             IEnumerable<User> users = Enumerable.Empty<User>();
-            
+
             IEnumerable<User> result = controller.CityQuery(users, "Salem");
             Assert.AreEqual(users.Count(), result.Count());
         }
->>>>>>> 8a832d019db39fa7ce981cdbc4a91825a8404ae3
     }
 }
