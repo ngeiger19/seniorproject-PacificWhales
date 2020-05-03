5867f40c2989891587684cc6c60509d55e5617ae
Nicole Geiger <nmgeiger19@mail.wou.edu>
2020-05-02 17:39:50 -0700

    finished musician part of veiwmodel

--- Sprint 1/Harmony/Models/ReccomendationViewModel.cs
+++ Sprint 1/Harmony/Models/ReccomendationViewModel.cs
@@ -6,14 +6,15 @@ using System.Web;
 using Harmony.Models;
 using Calendar.ASP.NET.MVC5.Models;
 using System.Web.Mvc;
+using Harmony.DAL;
 
 namespace Harmony.Models
 {
-    public class VenueReccomendationViewModel
+    public class VenueReccViewModel
     {
-        public VenueReccomendationViewModel() { }
+        public VenueReccViewModel() { }
 
-        public VenueReccomendationViewModel(Venue venue)
+        public VenueReccViewModel(Venue venue)
         {
             VenueName = venue.VenueName;
             Rating = venue.User.AveRating;
@@ -25,6 +26,40 @@ namespace Harmony.Models
         [StringLength(120)]
         [Display(Name = "Venue Name")]
         public string VenueName { get; set; }
+        public float? Rating { get; set; }
+
+        [StringLength(20)]
+        public string City { get; set; }
+
+        [StringLength(20)]
+        public string State { get; set; }
+
+        [Display(Name = "Shows Booked")]
+        public int ShowsBooked { get; set; }
+    }
+
+    public class MusicianReccViewModel
+    {
+        HarmonyContext db = new HarmonyContext();
+        public MusicianReccViewModel() { }
+        public MusicianReccViewModel(User user)
+        {
+            FirstName = user.FirstName;
+            LastName = user.LastName;
+            Rating = user.AveRating;
+            City = user.City;
+            State = user.State;
+            ShowsBooked = (from s in db.User_Show
+                           where s.MusicianID == user.ID
+                           select s).Count();
+        }
+        [StringLength(50)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
         public float? Rating { get; set; }
 
         [StringLength(20)]
