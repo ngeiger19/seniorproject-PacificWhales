c5c365e72e49b3baef1c8d55321f6cd150995e9d
Nicole Geiger <nmgeiger19@mail.wou.edu>
2020-05-02 17:31:05 -0700

    added reccomendation viewmodel

---
+++ Sprint 1/Harmony/Models/ReccomendationViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Harmony.Models;
+using Calendar.ASP.NET.MVC5.Models;
+using System.Web.Mvc;
+
+namespace Harmony.Models
+{
+    public class VenueReccomendationViewModel
+    {
+        public VenueReccomendationViewModel() { }
+
+        public VenueReccomendationViewModel(Venue venue)
+        {
+            VenueName = venue.VenueName;
+            Rating = venue.User.AveRating;
+            City = venue.City;
+            State = venue.State;
+            ShowsBooked = venue.Shows.Count();
+        }
+
+        [StringLength(120)]
+        [Display(Name = "Venue Name")]
+        public string VenueName { get; set; }
+        public float? Rating { get; set; }
+
+        [StringLength(20)]
+        public string City { get; set; }
+
+        [StringLength(20)]
+        public string State { get; set; }
+
+        [Display(Name = "Shows Booked")]
+        public int ShowsBooked { get; set; }
+    }
+}
